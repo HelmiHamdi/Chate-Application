a04687b5660a7f9eaa1a245298d26ddd34979422
Hamdi Helmi <helmihamdi977@gmail.com>
2025-07-30 12:36:13 +0100

    ce fait

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Augmente limite à 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor_react';
+            if (id.includes('lodash')) return 'vendor_lodash';
+            // Ajouter d'autres librairies si besoin
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
+});
